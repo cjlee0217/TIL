6b1914fc797cab06d12956ad06501c35768c3e00
Chanju Lee <chanju.lee0217@gmail.com>
2020-10-07 18:08:49 +0900

    R_programming: edited

--- R_practice01.R
+++ R_practice01.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(dplyr)
 mpg <- as.data.frame(ggplot2::mpg)
 mpg
 
@@ -38,9 +39,10 @@ mpga %>%
 
 # Q6.
 # "audi"에서 생산한 자동차 중 hwy가 1~5위에 해당하는 자동차 출력
-head(mpg %>% 
+mpg %>% 
   filter(manufacturer=="audi") %>% 
-  arrange(desc(hwy)),5)
+  arrange(desc(hwy)) %>% 
+  head(5)
 
 # Q7.
 # mpg 데이터 복사본을 만들고 
@@ -48,30 +50,38 @@ head(mpg %>%
 mpg_new <- mpg 
 mpg_new <- mpg_new %>% 
   mutate(cty_hwy=cty+hwy)
+mpg_new
 
 # Q8.
 # 앞에서 만든 '합산 연비 변수'를 2로 나눠 '평균 연비 변수'를 추가하세요.
 mpg_new <- mpg_new %>% 
   mutate(avg_fuelEff=cty_hwy/2)
+mpg_new
 
 # Q9-1.
 # '평균 연비 변수'가 가장 높은 자동차 3종의 데이터를 출력하세요.
-head(mpg_new %>% 
-  arrange(desc(avg_fuelEff)), 3) 
+mpg_new %>% 
+  arrange(desc(avg_fuelEff)) %>% 
+  head(3)
 
 # Q9-2.
 # 1~3번 문제를 해결할 수 있는 하나로 연결된 dplyr 구문을 만들어 출력하세요.
-head(mpg_new %>% 
+mpg_new %>% 
   mutate(Q9_2=(cty+hwy)/2) %>% 
-  arrange(desc(Q9_2)),3)
+  arrange(desc(Q9_2)) %>% 
+  head(3)
 
 # Q10.
 # 회사별로 "suv" 자동차의 도시 및 고속도로 통합 연비 평균을 구해 
 # 내림차순으로 정렬하고, 1~5위까지 출력하기
-head(mpg %>% 
+mpg %>% 
   group_by(manufacturer) %>% 
-  summarise(avg=mean(cty+hwy)) %>% 
-  arrange(desc(avg)),5)
+  filter(class=='suv') %>% 
+  mutate(total=(cty+hwy)/2) %>% 
+  summarise(avgtot=mean(total)) %>% 
+  arrange(desc(avgtot)) %>% 
+  head(5)
+
 
 # Q11.
 # class별 cty 평균을 구해보세요.
@@ -88,10 +98,11 @@ mpg %>%
 
 # Q13.
 # hwy 평균이 가장 높은 회사 세 곳을 출력하세요.
-head(mpg %>% 
+mpg %>% 
   group_by(manufacturer) %>% 
   summarise(mean_hwy=mean(hwy)) %>% 
-  arrange(desc(mean_hwy)),3)
+  arrange(desc(mean_hwy)) %>% 
+  head(3)
 
 # Q14.
 # 각 회사별 "compact" 차종 수를 내림차순으로 정렬해 출력하세요.
