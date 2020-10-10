b54d5cfb7d036d65c142469d6949cb6a89f1f3d6
Chanju Lee <chanju.lee0217@gmail.com>
2020-10-11 00:39:16 +0900

    Titanic_Kaggle_R: some exploratory analysis

--- titanic_kaggle.R
+++ titanic_kaggle.R
@@ -15,7 +15,36 @@ ggplot(data=train, aes(x=Survived))+geom_bar()
 
 # Sex column
 table(train$Sex, useNA='ifany')
-# Plot
-ggplot(data=train, aes(x=Sex))+geom_bar()
 
-       
+# Sex vs Survived
+sex_survived<-train %>% group_by(Sex, Survived) %>%  
+  summarise(Count=n()) %>% 
+  mutate(Ratio=Count/sum(Count))
+sex_survived
+
+ggplot(sex_survived, aes(x=Sex, y=Count, fill=factor(Survived)))+
+  geom_col(position='dodge')+
+  scale_fill_discrete('Survived')+
+  ggtitle("Sex vs Survived")
+
+ggplot(sex_survived, aes(x=Sex, y=Ratio, fill=factor(Survived)))+
+  geom_col(position='dodge')+
+  scale_fill_discrete('Survived')+
+  ggtitle("Sex vs Survived: Ratio")
+
+
+# Compare the average age of survived and not survived groups
+summary(train$Age)
+
+ggplot(train, aes(Age, fill=factor(Survived)))+
+  geom_histogram(bins=30)+
+  xlab('Age')+
+  scale_fill_discrete(name="Survived")+
+  ggtitle("Age vs Survived")
+
+train %>% filter(!is.na(Age)) %>% 
+  group_by(Survived) %>% 
+  summarise(mean(Age))
+# -> The survived group is younger (28.3) than the other group (30.6).
+
+
